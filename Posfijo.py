9d74d77d15f9372ad6b4044a690531416c2a0798
unknown <CISCO@CISCO-03.uagtabasco.edu.mx>
2015-02-20 13:13:11 -0600

    Corrida de escritorio, problemas al llenar pila con los operadores

--- Posfijo.py
+++ Posfijo.py
@@ -1,9 +1,9 @@
-N=100
+N=20
 #pila=[0 for i in range(N)]
 pila=[]
 
 global tope
-tope=0
+tope=-1
 
 def llena():
     if(tope==(N-1)):
@@ -40,28 +40,89 @@ def operando(i, EI):
             return True
     return False
 
-def operador(i, EI):
+def parentesis(i, EI):
     if(EI[i]!=')'):
         return True
+    else:
+        return False
+    
+#Ope    PriInf  PriPila
+#^      4       3
+#*      2       2
+#/      2       2
+#+      1       1
+#-      1       1
+#(      5       0
+#)      NONE    NONE
+    
+def infijo(i, EI):
+    if(EI[i]=='^'):
+        prioridadop=4
+        return prioridadop
+    elif(EI[i]=='*'):
+        prioridadop=2
+        return prioridadop
+    elif(EI[i]=='/'):
+        prioridadop=2
+        return prioridadop
+    elif(EI[i]=='+'):
+        prioridadop=1
+        return prioridadop
+    elif(EI[i]=='-'):
+        prioridadop=1
+        return prioridadop
+    elif(EI[i]=='('):
+        prioridadop=5
+        return prioridadop
+
+def pripila(pila):
+    if(pila[tope]=='^'):
+        prioridadpi=3
+        return prioridadpi
+    elif(pila[tope]=='*'):
+        prioridadpi=2
+        return prioridadpi
+    elif(pila[tope]=='/'):
+        prioridadpi=2
+        return prioridadpi
+    elif(pila[tope]=='+'):
+        prioridadpi=1
+        return prioridadpi
+    elif(pila[tope]=='-'):
+        prioridadpi=1
+        return prioridadpi
+    elif(pila[tope]=='('):
+        prioridadpi=0
+        return prioridadpi
+
 
 #eistring=raw_input('Ingrese Operacion sin espacios: ')
 #EI=list(eistring.lower())
-EI=['a','+']
+EI=['a','*','b','/','(','a','+','c',')']
 EP=[]
-for i in range(2):
+for i in range(len(EI)):
+    print 'PILA'
+    print pila
+    print 'EI'
+    print EI
+    print 'EP'
+    print EP
+    print '------------------'
     if(operando(i, EI)==True):
         EP.append(EI[i])
-    elif(operador(i, EI)==True):
+    elif(parentesis(i, EI)==True):
         if (tope==-1):
             push(EI[i])
-        #else:
+        elif(tope>=0):
+            if(infijo(i,EI)>pripila(pila)):
+                push(EI[i])
+                print 'lol'
+            
             
             
            
 
-print pila
-print EI
-print EP
+
 
 
        
